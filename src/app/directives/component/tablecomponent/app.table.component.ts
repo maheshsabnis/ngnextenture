8805018dbb2485df286a13b5574d3416d9d9a905
mahesh <sabnis_m@hotmail.com>
2023-06-12 16:59:50 +0530

    ww

--- src/app/directives/component/tablecomponent/app.table.component.ts
+++ src/app/directives/component/tablecomponent/app.table.component.ts
@@ -6,23 +6,27 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 
 export class TableComponent implements OnInit {
-  @Input() DataSource:Array<any>;
+  private _DataSource:Array<any>;
   @Input() CanDelete:boolean;
   @Input() CanPaginate:boolean;
   @Input() PageSize:number;
   @Input() CanSort:boolean;
+  colSpan:number;
   headers:Array<any>;
   constructor() {
-    this.DataSource = new Array<any>();
+    this._DataSource = new Array<any>();
     this.CanDelete = false;
     this.headers = new Array<any>();
+    this.colSpan = 0;
   }
 
   ngOnInit() {
-    if(this.DataSource !== undefined || this.DataSource.length > 0){
+    console.log('c');
+    if(this._DataSource !== undefined || this._DataSource.length > 0){
       // Read the first Records from Arrau
-      let firstObject = this.DataSource[0];
+      let firstObject = this._DataSource[0];
       this.headers = Object.keys(firstObject);
+      this.colSpan = this.headers.length;
     }
     else {
       this.headers =[]; /* No Columns */
@@ -35,4 +39,14 @@ export class TableComponent implements OnInit {
   /*
     The Change EVent that will show next-page data from DataSource
   */
+
+    @Input()
+    set DataSource(v:Array<any>){
+      console.log('Set Updated...');
+      this._DataSource = v;
+    }
+    get DataSource():Array<any>{
+      console.log('Get Updated...');
+      return this._DataSource;
+    }
 }
